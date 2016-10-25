ceac56ac3d46c52baf096493eed1d423ff76a69e
Alex Norton <alexnorton@users.noreply.github.com>
2016-10-25 11:54:36 +0100

    Add TranscriptSegment helper methods (#2) - closes #1
    * run tests in interactive mode
    
    * add eslint config
    
    * add tests for new TranscriptSegment methods
    
    * implement TranscriptSegment helper methods

--- src/TranscriptSegment.js
+++ src/TranscriptSegment.js
@@ -6,7 +6,17 @@ const TranscriptSegmentRecord = new Immutable.Record({
 });
 
 class TranscriptSegment extends TranscriptSegmentRecord {
+  getStart() {
+    return this.words.first().get('start');
+  }
 
+  getEnd() {
+    return this.words.last().get('end');
+  }
+
+  getText() {
+    return this.words.map(w => w.get('text')).join(' ');
+  }
 }
 
 export default TranscriptSegment;
